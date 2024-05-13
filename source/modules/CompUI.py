5b00d26ba39e65c1d5d49a8bbd5010fe2ec1fa80
`Asterix <imagineer@imagineer.net.au>
2024-05-13 17:10:38 +1000

    2022 updates
    Fix presetsext and compui error which requires locked script chop
    Fix routing errors in dat execute for all routers including
    master, aux, scenes, videoIn, altChannels
    Fix missing uOutputLevel reference on mixWithBlend.glsl

--- source/modules/CompUI.py
+++ source/modules/CompUI.py
@@ -1,7 +1,7 @@
 class CompUI(object):
 
 	def __init__(self,ownerComp):
-	
+
 		self.ownerComp = ownerComp
 		self.path = self.ownerComp.path
 
@@ -14,27 +14,28 @@ class CompUI(object):
 			self.ownerComp.store('UIAttrTblUI', self.StoreComp.op('uiAttr'))
 			self.ownerComp.store('ModSources', self.StoreComp.op('modSources'))
 
-		self.ownerComp.store('CtrlsPath', self.ownerComp.path)	
+		self.ownerComp.store('CtrlsPath', self.ownerComp.path)
 		self.UIAttrTbl = self.ownerComp.fetch('UIAttrTbl', self.StoreComp.op('uiAttr'))
 
 		self.ParNames = [r[0].val for r in self.UIAttrTbl.rows()[1:]]
 		self.NumGadgets = len(self.ParNames)
 
 		self.CtrlsParNames = [par.name for par in self.ownerComp.pars()]
-
+		if self.StoreComp.op("modRouter/default_values"):
+			self.StoreComp.op("modRouter/default_values").lock = True
 		if 'Haspresets' in self.CtrlsParNames:
 
 			if self.ownerComp.par.Haspresets.eval():
 
 				self.Presets = self.Plugin.op('presets')
 				self.PresetControls = self.ownerComp.op('../presetControls')
-				self.PresetCtrls = self.ownerComp.op('presets')	
-	
+				self.PresetCtrls = self.ownerComp.op('presets')
+
 	def Setupscroll(self, *args):
 		scrollW = 12
-		
+
 		list = self.ownerComp.op('controlsView')
-		windowW = int(self.ownerComp.par.w) 
+		windowW = int(self.ownerComp.par.w)
 		windowH = int(self.ownerComp.par.h)
 		listW = list.par.w
 		listH = list.par.h
@@ -53,12 +54,12 @@ class CompUI(object):
 				windowH -= self.ownerComp.op('presets').par.h
 		except:
 			pass
-		
+
 		self.ownerComp.store('ListHeight', listH)
 		self.ownerComp.store('WindowHeight',windowH)
 
 		if windowH < listH:
-			
+
 			listW = windowW - scrollW - 2
 			dispScroll = True
 
@@ -83,12 +84,12 @@ class CompUI(object):
 		compAttr = storeComp.fetch('CompAttr')
 		#compAttr['fullListHeight',1] = listH
 		scroll.op('setup').run(settings)
-		
+
 		if dispScroll == False:
 			scroll.op('slider').panel.v = .5
 		else:
 			scroll.op('slider').panel.v = 1
-			
+
 	def Setupui(self, *args, setupScroll = True):
 		allParmMaps = self.ownerComp.op(self.ownerComp.fetch('ALL_PARM_MAPS'))
 		ctrlsPath = self.ownerComp.fetch('CtrlsPath')
@@ -104,24 +105,24 @@ class CompUI(object):
 
 
 		for r in uiAttr.rows()[1:]:
-			
+
 			settings = {'width': width, 'height': 20, 'fieldWidth': 50, 'labelWidth': labelWidth, 'scrollWidth': 12, 'listHeight': 140 }
 
 			gadget = self.ownerComp.op(r[0])
 
 			attr = gadget.fetch('attr')
-			
+
 			displayName = attr['displayName']
 			format = attr['format']
-			
+
 			settings['label'] = displayName
-			settings['format'] = format	
-			
+			settings['format'] = format
+
 			self.ownerComp.op(r[0] + '/setup').run(settings)
 
 		if setupScroll:
 			self.ownerComp.Setupscroll()
-		
+
 		if 'Haspresets' in self.CtrlsParNames:
 
 			if self.ownerComp.par.Haspresets.eval():
@@ -164,7 +165,7 @@ class CompUI(object):
 				self.PresetCtrls.par.Presetcomp = '../plugin/presets'
 				self.PresetCtrls.par.Presetcontrols = '../presetControls'
 
-		
+
 	def Createcompui(self, *args, reCreate = True):
 
 		if reCreate:
@@ -179,10 +180,10 @@ class CompUI(object):
 		parDest = self.ownerComp.op(self.ownerComp.fetch('Parameters'))
 		storeComp = self.ownerComp.fetch('StoreComp')
 		uiAttrTble = self.ownerComp.fetch("UIAttrTbl")
-		
+
 		#update the parameters table if any new uiAttr entries are made
 		#we store them in rows[] to ensure current parameters / global mods aren't overwritten so we don't have to reset those everytime Createcompui is called
-		
+
 		#update these so we don't get empty export blips.
 		rows = []
 		hasDefaultValuesOP = False
@@ -197,37 +198,38 @@ class CompUI(object):
 		for r in uiAttrTble.rows()[1:]:
 
 			if hasDefaultValuesOP:
+				defaultValues.lock = True
 				defaultValues.appendChan(r[0].val)
 				defaultValues[r[0].val][0] = r[4].val
 
 			try:
 				rows.append[parDest.row(r[0])]
-				
+
 			except:
 				rows.append([r[0],r[4],'','','',''])
 
 		if hasDefaultValuesOP:
 
 			defaultValues.lock = True
-				
+
 		parDest.clear(keepFirstRow=True)
 		for r in rows:
 			parDest.appendRow(r)
-		
+
 		#get and store type, name, outputs, uiPath, presetControl
 		compAttr = {'attr': {}, 'uiAttr': {}}
-		compPar = {'values': {}} 
-		
+		compPar = {'values': {}}
+
 		tCompAttr = self.ownerComp.op(self.ownerComp.fetch('CompAttrTbl'))
 
-		for r in tCompAttr.rows():	
+		for r in tCompAttr.rows():
 			try: val = eval(str(r[1]))
 			except: val = str(r[1])
 			compAttr['attr'][str(r[0])] =  val
 
 		self.ownerComp.op('label/text').par.text = tCompAttr['name', 1].val
 
-		#get components UI attributes from table	
+		#get components UI attributes from table
 		t = self.ownerComp.fetch('UIAttrTbl')
 
 		#get list of attribute names
@@ -237,7 +239,7 @@ class CompUI(object):
 
 		#create gadget for each parameter in attribute table
 		for r in t.rows()[1:]:
-			
+
 			gadgetName = str(r[0])
 			gadgetType = str(r[5])
 
@@ -253,7 +255,7 @@ class CompUI(object):
 			gadget.par.display = True
 			gadget.par.reinitextensions.pulse()
 
-			
+
 			controlsView.outputCOMPConnectors[0].connect(gadget)
 
 			initGadgetAttr = gadget.op('initAttributes')
@@ -274,7 +276,7 @@ class CompUI(object):
 					elif attr[1] == 'bool':
 						attrVal = bool(t[r[0].row, str(attr[0])])
 					gadgetAttr[str(attr[0])] = attrVal
-				
+
 				#if the attribute doesn't exist in component table get initVal from gadget table and store
 				else:
 
@@ -310,9 +312,9 @@ class CompUI(object):
 					gadgetPar[str(par[0])] = int(par[2])
 				elif par[1] == 'bool':
 					gadgetPar[str(par[0])] = bool(par[2])
-			
-			gadgetPar['value'] = gadgetAttr['default']	
-			
+
+			gadgetPar['value'] = gadgetAttr['default']
+
 
 			gadget.op('setUI').run(gadgetPar, delayFrames = 1)
 
@@ -320,7 +322,7 @@ class CompUI(object):
 				parDest[gadgetName,par[0]] = par[1]
 
 			compPar['values'][gadgetName] = gadgetPar
-				
+
 		storeComp.store('CompAttr',compAttr)
 		storeComp.store('CompPar',compPar)
 		self.ownerComp.store('UIAttr', compAttr['uiAttr'])
@@ -331,8 +333,8 @@ class CompUI(object):
 		#me.fetch("ROOT").Delay(delayFrames = 2).Call(self.ownerComp, 'Setupui', [])
 
 		self.Setupui()
-		self.ownerComp.par.reinitextensions.pulse()		
-		
+		self.ownerComp.par.reinitextensions.pulse()
+
 	def Hide(self, *args):
 
 		if self.ownerComp.fetch('IsDisplayed') == 0:
@@ -341,7 +343,7 @@ class CompUI(object):
 				gadget = self.ownerComp.op(parName)
 				gadget.par.display = 0
 
-				
+
 	def Display(self, *args, display = True, setUI = True):
 
 		self.ownerComp.store('GadgetCount', 1)
@@ -373,7 +375,7 @@ class CompUI(object):
 	def Plugin(self, value):
 		if 'Plugin' in self.ownerComp.pars():
 			self.ownerComp.par.Plugin = value
-		
+
 	@property
 	def StoreComp(self):
 		if 'Storecomp' in self.ownerComp.pars():
